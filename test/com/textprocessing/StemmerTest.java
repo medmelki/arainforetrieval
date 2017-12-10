065476890aca75cb269a3b2c2fa918614a78462c
mmelki <mmelki@kapit.fr>
2017-12-10 21:30:03 +0100

    Add Stemmer function

---
+++ test/com/textprocessing/StemmerTest.java
@@ -0,0 +1,23 @@
+package com.textprocessing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StemmerTest {
+
+    @Test
+    public void testStemm() {
+
+        Stemmer stemmer = new Stemmer();
+
+        String root = stemmer.findRoot("الإسهال");
+        assertEquals("سهل", root);
+
+        root = stemmer.findRoot("الحاد");
+        assertEquals("لحد", root);
+
+        root = stemmer.findRoot("المزمن");
+        assertEquals("زمن", root);
+    }
+}
