12dda291b4d0aae1021b3b4386c04de03a7541d2
mmelki <mmelki@kapit.fr>
2017-12-12 14:34:27 +0100

    Add Tokenizer & stopWordsRemover function

--- src/com/textprocessing/util/TextUtility.java
+++ src/com/textprocessing/util/TextUtility.java
@@ -1,5 +1,55 @@
 package com.textprocessing.util;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+
 public class TextUtility {
 
+    private static List<String> arabicStopWords = new ArrayList<>();
+
+    static {
+        try {
+            BufferedReader bf = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream("assets/segmenter/stop-words-arabic.txt"), "UTF8"));
+
+            String str;
+            while ((str = bf.readLine()) != null) {
+                arabicStopWords.add(str.trim());
+            }
+            bf.close();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String removeStopWords(String s) {
+        List<String> tokens = TextUtility.tokenize(s);
+        for (String stopWord : arabicStopWords) {
+            Iterator<String> iter = tokens.iterator();
+            while (iter.hasNext()) {
+                String token = iter.next();
+                if (token.equals(stopWord))
+                    iter.remove();
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (String t : tokens)
+            result.append(t).append(" ");
+        return result.toString().trim();
+    }
+
+    public static List<String> tokenize(String s) {
+        String[] tokens = s.split("\\s*(=>|,|\\s)\\s*");
+        List<String> listTokens = new ArrayList<>();
+        Collections.addAll(listTokens, tokens);
+        return listTokens;
+    }
 }
+
--- test/com/textprocessing/TextUtilityTest.java
+++ test/com/textprocessing/TextUtilityTest.java
@@ -1,13 +1,28 @@
 package com.textprocessing;
 
+import com.textprocessing.util.TextUtility;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class TextUtilityTest {
 
     @Test
-    private void testTokenize(String input) {
-        assertEquals(0, 0);
+    public void testTokenize() {
+        List<String> stringList = TextUtility.tokenize("الإسهال الحاد والمزمن");
+        assertArrayEquals(new String[]{"الإسهال", "الحاد", "والمزمن"}, stringList.toArray());
+        // verify Encoding
+        assertNotEquals(Arrays.toString(new String[]{"???????", "?????", "???????"}), Arrays.toString(stringList.toArray()));
+    }
+
+    @Test
+    public void testRemoveStopWords() {
+        String str = TextUtility.removeStopWords("بعض الإسهال الحاد و المزمن");
+        assertEquals("الإسهال الحاد المزمن", str);
     }
 }
